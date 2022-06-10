62ec47caf736173196d3af846138427907c7cabd
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-06-10 16:46:45 +0200

    Add get methods

--- express_refactor/server_express.js
+++ express_refactor/server_express.js
@@ -3,3 +3,12 @@ const app = express();
 app.use(express.static("public"));
 
 app.listen(5000);
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/:page", (request, response) => {
+  let pageName = request.params.page;
+  response.sendFile(__dirname + `/${pageName}.html`);
+});
