fec871410210626af060bba482864c94290ff9e1
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-06-11 15:11:25 +0200

    Improve app.listen

--- express_refactor/server_express.js
+++ express_refactor/server_express.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.static("public"));
-
-app.listen(5000);
+const PORT = 5000;
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
@@ -11,4 +10,8 @@ app.get("/", (request, response) => {
 app.get("/:page", (request, response) => {
   let pageName = request.params.page;
   response.sendFile(__dirname + `/${pageName}.html`);
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
